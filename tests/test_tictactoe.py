89b253543a4228036ef3cdbb703070c3ba4413ad
volodymyr <zalorderon3331@gmail.com>
2022-12-22 18:50:28 +0200

    reworked pylint in ci

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,3 +1,4 @@
+# pylint: disable=redefined-outer-name
 """This module tests MVC structure of tictactoe module"""
 from pytest import fixture
 from tictactoe_mvc import Field
@@ -5,54 +6,64 @@ from tictactoe_mvc import Field
 
 @fixture
 def field_object() -> Field:
+    """Returns fixture for field object"""
     return Field(9)
 
 
 def test_check_all(field_object: Field) -> None:
+    """Tests check all function"""
     assert field_object.check_all(' ', 9)
 
 
 def test_check_all_wrong(field_object: Field) -> None:
+    """Tests check all function"""
     assert not field_object.check_all('X', 9)
 
 
 def test_check_column(field_object) -> None:
+    """Tests check all column"""
     for i in [0, 3, 6]:
         field_object.field[i] = 'X'
     assert field_object.check_column('X', 3)
 
 
 def test_check_column_false(field_object) -> None:
+    """Tests check column"""
     for i in [0, 3, 5]:
         field_object.field[i] = 'X'
     assert not field_object.check_column('X', 3)
 
 
 def test_check_row(field_object) -> None:
+    """Tests check row"""
     for i in [0, 1, 2]:
         field_object.field[i] = 'X'
     assert field_object.check_row('X', 3)
 
 
 def test_check_row_false(field_object) -> None:
+    """Tests check row"""
     for i in [0, 1, 3]:
         field_object.field[i] = 'X'
     assert not field_object.check_row('X', 3)
 
 
 def test_check_diag_false(field_object) -> None:
+    """Tests check diag function"""
     for i in [0, 4, 7]:
         field_object.field[i] = 'X'
     assert not field_object.check_diagonal('X', 3)
 
 
 def test_check_diag(field_object) -> None:
+    """Tests check diag function"""
     for i in [0, 4, 8]:
         field_object.field[i] = 'X'
     assert field_object.check_diagonal('X', 3)
 
 
 def test_get_possible_moves(field_object) -> None:
+    """Tests get possible moves function"""
     values = [0, 4, 8]
     for i in values:
         field_object.field[i] = 'X'
@@ -61,5 +72,6 @@ def test_get_possible_moves(field_object) -> None:
 
 
 def test_make_move(field_object: Field) -> None:
+    """Tests check make_move function"""
     field_object.make_move(2, 'X')
     assert field_object.last_play == 2
